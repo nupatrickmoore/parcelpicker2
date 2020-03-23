7e4b077bf159ba2a8ca13842a7bd73ebb1feabfe
patrickmoore <patrick.moore4747@gmail.com>
2020-03-23 10:37:35 -0400

    Added node402 attempt

--- parcelpicker2.py
+++ parcelpicker2.py
@@ -1,27 +1,41 @@
 import canopen
-import can
-
-# define bus instance
-# bus = can.interface.Bus(bustype='socketcan', channel='vcan0', bitrate=250000)
+from canopen.profiles.p402 import BaseNode402
 # define network
 network = canopen.Network()
 # establish connection
-network.connect(channel='vcan0', bustype='socketcan')  # start communication
+network.connect(channel='vcan0', bustype='socketcan')  # start communication (virtual port)
 
-# standard way to add node to network, there is an alternative to scan for the node
-# node = network.add_node(6, '/path/to/object_dictionary.eds')
-# local_node = canopen.LocalNode(1, '/path/to/master_dictionary.eds')
+# # standard way to add node to network, there is an alternative to scan for the nodes
+# node = network.add_node(6, 'C5-E-1-09.eds')
+# local_node = canopen.LocalNode(1, 'C5-E-1-09.eds')
 # network.add_node(local_node)
 
-# once .eds is provided, we can test if it can be read here
-# node = network.add_node(6, 'od.eds')
+# add node to network with device profile for motor controller
+node = canopen.BaseNode402(3, 'C5-E-1-09.eds')
+network = canopen.Network()
+network.add_node(node)
+
+# .eds Test block
+# node = network.add_node(6, 'C5-E-1-09.eds')
 # for obj in node.object_dictionary.values():
 #     print('0x%X: %s' % (obj.index, obj.name))
 # if isinstance(obj, canopen.objectdictionary.Record):
 #     for subobj in obj.values():
 #         print(' %d: %s' % (subobj.subindex, subobj.name))
 
-network.disconnect() # disconnect after use
+# # Send NMT start to all nodes (exclude for virtual port
+# network.send_message(0x0, [0x1, 0])
+# node.nmt.wait_for_heartbeat()
+# assert node.nmt.state == 'OPERATIONAL' # PDOs can only be used in this state
 
+# run the setup routine for TPDO1 and it's callback
+node.setup_402_state_machine()
 
+# command to go to 'READY TO SWITCH ON' from 'NOT READY TO SWITCH ON' or 'SWITCHED ON'
+node.sdo[0x6040].raw = 0x06
 
+# Read the state of the Statusword
+node.sdo[0x6041].raw
+
+# disconnect after use
+# network.disconnect()
