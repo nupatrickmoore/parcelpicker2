e97022b54da4b1dd820ef025019f8ab3f8b05fc3
Patrick <moore.pat@husky.neu.edu>
2020-05-08 13:35:46 -0400

    Added tests, pdos, PCANBasic

--- parcelpicker2.py
+++ parcelpicker2.py
@@ -1,41 +1,52 @@
 import canopen
-from canopen.profiles.p402 import BaseNode402
-# define network
-network = canopen.Network()
-# establish connection
-network.connect(channel='vcan0', bustype='socketcan')  # start communication (virtual port)
+import time
+network = canopen.Network() # define network
+network.connect(channel='PCAN_USBBUS1', bustype='pcan')  # establish communication
 
-# # standard way to add node to network, there is an alternative to scan for the nodes
-# node = network.add_node(6, 'C5-E-1-09.eds')
-# local_node = canopen.LocalNode(1, 'C5-E-1-09.eds')
-# network.add_node(local_node)
+# Add node to network TODO: scan for and add node
 
-# add node to network with device profile for motor controller
-node = canopen.BaseNode402(3, 'C5-E-1-09.eds')
-network = canopen.Network()
-network.add_node(node)
+node = network.add_node(1, 'C5-E-1-09.eds')
+local_node = canopen.LocalNode(1, 'C5-E-1-09.eds')
+network.add_node(local_node)
+local_node = network[1]
 
-# .eds Test block
-# node = network.add_node(6, 'C5-E-1-09.eds')
-# for obj in node.object_dictionary.values():
-#     print('0x%X: %s' % (obj.index, obj.name))
-# if isinstance(obj, canopen.objectdictionary.Record):
-#     for subobj in obj.values():
-#         print(' %d: %s' % (subobj.subindex, subobj.name))
+local_node.rpdo.read()
+local_node.tpdo.read()
 
-# # Send NMT start to all nodes (exclude for virtual port
-# network.send_message(0x0, [0x1, 0])
-# node.nmt.wait_for_heartbeat()
-# assert node.nmt.state == 'OPERATIONAL' # PDOs can only be used in this state
+# Enter operational state (NMT command)
+# PDOs must be configured in the "Pre-Operational" NMT state
+local_node.nmt.send_command(0x01)     # enter operational state (required for PDOs)
 
-# run the setup routine for TPDO1 and it's callback
-node.setup_402_state_machine()
 
-# command to go to 'READY TO SWITCH ON' from 'NOT READY TO SWITCH ON' or 'SWITCHED ON'
-node.sdo[0x6040].raw = 0x06
+# Set mode of operation to profile position
+# change 0x6060 register from 0 to 1 (dec)
 
-# Read the state of the Statusword
-node.sdo[0x6041].raw
+local_node.pdo['0x6060'].set_data(b'\x01')
 
-# disconnect after use
-# network.disconnect()
+print(local_node.pdo['0x6060'].get_data())
+
+# # test to see if command worked by reading 0x6061 (1 for profile position) (may have to be tdpo)
+operation_mode = local_node.sdo[0x6061].get_data()
+print(operation_mode)
+
+# Set position type to relative
+# change bit 6 of 0x6040 to 1
+
+local_node.pdo['0x6040'].set_data(bytes([0x02, 0x00]))
+print(local_node.pdo['0x6040'].get_data())
+
+# set velocity value (int)
+# Target velocity is stored in 0x60FF
+# local_node.sdo['0x60FF'].set_data(1)
+
+# set position value (int)
+# target position is stored in 0x607A
+# relative position is stored in 0x60F2
+local_node.sdo['0x607F'].set_data(1)
+# Start travel command
+# carried out on the transition of bit 4 of 0x6040 from 0 to 1
+
+# Change set point immediately
+# bit 5 of 0x6040 controls whether the travel command starts immediately or waits for previous to complete
+
+network.disconnect()      # disconnect after use
