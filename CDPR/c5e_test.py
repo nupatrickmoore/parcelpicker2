e8a67dd57eae9fa236cdaddc9f2985297940b244
Patrick <moore.pat@husky.neu.edu>
2020-05-20 11:32:23 -0400

    Committing changes from testing

--- CDPR/c5e_test.py
+++ CDPR/c5e_test.py
@@ -1,5 +1,8 @@
 import c5e
 import canopen
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
 
 network = canopen.Network()     # define network
 network.connect(channel='PCAN_USBBUS1', bustype='pcan', bitrate=1000000)  # establish communication
@@ -9,7 +12,7 @@ driver = c5e.Driver(network, 1)
 driver.enable()
 
 print("Moving")
-driver.goto(3000, speed=50)
+driver.goto(3000, speed=100)
 print("Done")
 
 driver.shutdown()
